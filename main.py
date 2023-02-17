96904613b7397640e1a87805c0db40f65e7584a9
nhlimon12 <nhlimon4554@gmail.com>
2023-02-17 16:12:53 +0600

    FastAPI Journey

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def index():
     return "Hello there!🍕"
 
 
-@app.get('/about')
-def about():
-    return {'data': {'name': 'About Page'}}
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': 'Blog - ' + str(id)}
+
+
+@app.get('/blog/{blog_id}/comments/{comment_id}')
+def comment(blog_id: int, comment_id: int):
+    return {'data': 'Blog ID ' + str(blog_id) + ' and Comment ID ' + str(comment_id)}
